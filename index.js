bb8e674e1ebdb552e3c4414d3c675c66a2e11d01
Phorn Horm <natophorn@gmail.com>
2026-02-23 11:42:08 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const TelegramBot = require("node-telegram-bot-api");
+
+const TOKEN = "8658959369:AAGGAMiDzMF9BNr_0HGy_RQl3BW2QTn1hQQ";
+
+const bot = new TelegramBot(TOKEN);
+const app = express();
+
+app.use(express.json());
+
+app.post("/webhook", (req, res) => {
+
+    const update = req.body;
+
+    if(update.message && update.message.new_chat_members){
+
+        update.message.new_chat_members.forEach(user => {
+
+            const name = user.first_name;
+
+            bot.sendMessage(
+                update.message.chat.id,
+                `សូមស្វាគមន៍ ${name} មកកាន់ channel genz របស់យើង`
+            );
+
+        });
+    }
+
+    res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Bot server running");
+});
